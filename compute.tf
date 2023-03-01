638866328eef7728e351c7346020c8355e1feb69
Gusa27 <morales.busat@gmail.com>
2023-03-01 15:11:16 +0100

    v1

---
+++ compute.tf
@@ -0,0 +1,16 @@
+resource "aws_network_interface" "pc01-eth0" {
+  description = "pc01-port1"
+  subnet_id   = aws_subnet.pcsubnetaz1.id
+}
+
+resource "aws_instance" "pc01" {
+    ami = "ami-065793e81b1869261"
+    instance_type = "t2.micro"
+    key_name          = var.keyname
+    availability_zone = var.az1
+
+  network_interface {
+    network_interface_id = aws_network_interface.pc01-eth0.id
+    device_index         = 0
+  }
+}
