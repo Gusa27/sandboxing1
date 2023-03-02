4b3a3ae872ea79437030778a1fadd6884a464dee
Gusa27 <morales.busat@gmail.com>
2023-03-02 13:41:23 +0100

    v1

--- compute.tf
+++ compute.tf
@@ -1,24 +1,21 @@
 resource "aws_instance" "bastion" {
-    ami = "ami-065793e81b1869261"
+    ami = "ami-017661010558cc954"
     instance_type = "t3.xlarge"
-    key_name          = var.keyname
+    key_name = var.keyname
     availability_zone = var.az1
     associate_public_ip_address = "true"
-    subnet_id   = aws_subnet.bastionsubnetaz1.id
+    subnet_id = aws_subnet.bastionsubnetaz1.id
     vpc_security_group_ids = ["${aws_security_group.bastion.id}"]
 }
 
 resource "aws_instance" "pc01" {
     ami = "ami-0acafadf3f00e9c70"
     instance_type = "t3.xlarge"
-    key_name          = var.keyname
+    key_name = var.keyname
     availability_zone = var.az1
     associate_public_ip_address = "false"
-    subnet_id   = aws_subnet.pcsubnetaz1.id
+    subnet_id = aws_subnet.pcsubnetaz1.id
 }
-
-
-
 
 resource "aws_security_group" "bastion" {
   name        = "bastion-sg"
