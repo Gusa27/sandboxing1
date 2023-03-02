db8e260486664d8992f26b1441cd2d653d8e1796
Gusa27 <morales.busat@gmail.com>
2023-03-02 13:12:22 +0100

    v1

--- compute.tf
+++ compute.tf
@@ -1,7 +1,6 @@
 resource "aws_network_interface" "bastion-eth0" {
   description = "pc01-port1"
   subnet_id   = aws_subnet.pcsubnetaz1.id
-  associate_public_ip_address = "true"
 }
 
 resource "aws_network_interface" "pc01-eth0" {
@@ -14,12 +13,7 @@ resource "aws_instance" "bastion" {
     instance_type = "t3.xlarge"
     key_name          = var.keyname
     availability_zone = var.az1
-
-  network_interface {
-    network_interface_id = aws_network_interface.bastion-eth0.id
-    device_index         = 0
-  }
-}
+    associate_public_ip_address = "true"
 
 resource "aws_instance" "pc01" {
     ami = "ami-0acafadf3f00e9c70"
