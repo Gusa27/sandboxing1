6e5d2f499deff1917881157b9be60076fecabb37
Gusa27 <morales.busat@gmail.com>
2023-03-02 14:30:05 +0100

    v1

--- compute.tf
+++ compute.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "bastion" {
-    ami = "ami-017661010558cc954"
-    instance_type = "t3.xlarge"
-    key_name = var.keyname
-    availability_zone = var.az1
+    ami                         = "ami-017661010558cc954"
+    instance_type               = var.size
+    key_name                    = var.keyname
+    availability_zone           = var.az1
     associate_public_ip_address = "true"
-    subnet_id = aws_subnet.bastionsubnetaz1.id
-    vpc_security_group_ids = ["${aws_security_group.bastion.id}"]
-    iam_instance_profile = "EC2_to_SSM"
+    subnet_id                   = aws_subnet.bastionsubnetaz1.id
+    vpc_security_group_ids      = ["${aws_security_group.bastion.id}"]
+    iam_instance_profile        = "EC2_to_SSM"
 
   tags = {
     Name = "BastionServer"
@@ -14,13 +14,13 @@ resource "aws_instance" "bastion" {
 }
 
 resource "aws_instance" "pc01" {
-    ami = "ami-0acafadf3f00e9c70"
-    instance_type = "t3.xlarge"
-    key_name = var.keyname
-    availability_zone = var.az1
+    ami                         = "ami-0acafadf3f00e9c70"
+    instance_type               = var.size
+    key_name                    = var.keyname
+    availability_zone           = var.az1
     associate_public_ip_address = "false"
-    subnet_id = aws_subnet.pcsubnetaz1.id
-    vpc_security_group_ids = ["${aws_security_group.pc.id}"]
+    subnet_id                   = aws_subnet.pcsubnetaz1.id
+    vpc_security_group_ids      = ["${aws_security_group.pc.id}"]
 
   tags = {
     Name = "Windows10"
