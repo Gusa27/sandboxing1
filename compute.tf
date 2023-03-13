c4b1d0dc3442bdc0ce9680137982afd5ad867140
Gusa27 <morales.busat@gmail.com>
2023-03-13 16:18:44 +0100

    v1

--- compute.tf
+++ compute.tf
@@ -27,6 +27,20 @@ resource "aws_instance" "pc01" {
   }
 }
 
+resource "aws_instance" "dc-ficosa" {
+    ami                         = "ami-0876379a8678f997f"
+    instance_type               = var.size
+    key_name                    = var.keyname
+    availability_zone           = var.az1
+    associate_public_ip_address = "false"
+    subnet_id                   = aws_subnet.dcsubnetaz1.id
+    vpc_security_group_ids      = ["${aws_security_group.dc.id}"]
+
+  tags = {
+    Name = "DC-Ficosa"
+  }
+}
+
 resource "aws_security_group" "bastion" {
   name        = "bastion-sg"
   description = "Bastion Security Group"
@@ -47,6 +61,26 @@ resource "aws_security_group" "bastion" {
 }
 
 resource "aws_security_group" "pc" {
+  name        = "pc-sg"
+  description = "PC Security Group"
+  vpc_id      = aws_vpc.fgtvm-vpc.id
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    #cidr_blocks = var.vpccidr
+    cidr_blocks = ["${var.vpccidr}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
+}
+
+resource "aws_security_group" "dc" {
   name        = "pc-sg"
   description = "PC Security Group"
   vpc_id      = aws_vpc.fgtvm-vpc.id
